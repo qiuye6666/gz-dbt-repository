8c5118d55fd67acc5c3247403f1474d5b292ad6f
Ye Qiu <yee.qiu@gmail.com>
2024-11-16 10:57:27 +0000

    description sales

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -11,7 +11,7 @@ renamed as (
     select
         date_date,
         orders_id,
-        CAST (ptd_id AS STRING) AS products_id
+        pdt_id AS products_id,
         revenue,
         quantity
 
--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,10 +11,9 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-        shipping_fee_1,
         logcost,
-        ship_cost
-
+        CAST(ship_cost AS FLOAT64) AS ship_cost
+    
     from source
 
 )
